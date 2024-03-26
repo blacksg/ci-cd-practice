618d44dd7890b42d0c33d2421b2553fb95aa72c7
blacksg <sagwa.nth@gmail.com>
2024-03-26 14:25:19 +0900

    Create project

---
+++ src/test/java/kamang/cicdpractice/CicdPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kamang.cicdpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
